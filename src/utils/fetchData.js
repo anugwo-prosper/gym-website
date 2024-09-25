ae569c2972cb424ce69b5a0e33dffb2b17b56f75
Anugwo Prosper Godwin <anugwoprosper777@gmail.com>
2024-09-25 01:24:56 +0100

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a61686d4bfmsh5276bce21b602d1p1bacf2jsn5d9ace2e70ad',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a61686d4bfmsh5276bce21b602d1p1bacf2jsn5d9ace2e70ad',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
